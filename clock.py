f12410b653876b585ca28240f799e3976a3a1da5
pcn-rapi <rapi@progcode.co>
2017-02-20 08:33:58 -0500

    Make it scheduled

--- clock.py
+++ clock.py
@@ -1,9 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from etl.indivisible import main as indivisible
+
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=60)
 def timed_job():
     print('This job is run every minute')
+    try:
+        indivisible.run()
+    except ValueError as error:
+        print('Caught this error: ' + repr(error))
+
     
 sched.start()
