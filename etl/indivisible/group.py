a956c7e4ae1d6e99a5f536d37cd54efd1c91ca3b
pcn-rapi <rapi@progcode.co>
2017-02-20 11:33:25 -0500

    Test

--- etl/indivisible/group.py
+++ etl/indivisible/group.py
@@ -67,7 +67,7 @@ def retrieve_and_clean_data():
     
     response = urllib.urlopen(indivisible_url)
     with open('data/indivisible.csv', 'w') as f:
-        f.write(str(response.read().decode('utf-8'), encoding='utf-8', errors = 'ignore'))
+        f.write(response.read().decode('utf-8').encode('ascii', 'ignore'))
 
     # read indivisble
     indivisible_groups = []
