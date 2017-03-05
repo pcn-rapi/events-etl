f20b3b13c290fd0ca1e4f9b4b913eb79585240b0
pcn-rapi <rapi@progcode.co>
2017-03-04 19:24:37 -0500

    People Power

--- clock.py
+++ clock.py
@@ -6,12 +6,12 @@ from etl.peoplepower import main as peoplepower
 from rq import Queue
 from worker import conn
 
-q = Queue(connection=conn)
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=60)
 def timed_job():
     print(' This job runs every 60 minutes')
+    q = Queue(connection=conn)
     result = q.enqueue(peoplepower.queue)
 
 sched.start()
