876e40c9b41e1a53ed68406feed6870ce6659760
pcn-rapi <rapi@progcode.co>
2017-03-14 20:02:08 -0400

    Updates to people power

--- clock.py
+++ clock.py
@@ -13,7 +13,8 @@ def timed_job():
     print(' This job runs every 60 minutes')
     q = Queue(connection=conn)
     result = q.enqueue(peoplepower.queue)
-    print('Result is %d ' result)
-    result = q.enqueue(indivisible.queue)
+    print('Result is %d ' % result)
+    result2 = q.enqueue(indivisible.queue)
+    print('Result2 is %d ' % result2)
 
 sched.start()
