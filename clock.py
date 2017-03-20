84989d5e9a17bb11c58c69a5d58425b290717432
pcn-rapi <rapi@progcode.co>
2017-03-20 10:10:33 -0400

    Changes

--- clock.py
+++ clock.py
@@ -9,13 +9,13 @@ from worker import conn
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', minutes=30)
 def timed_for_aclu():
     print('Running Job for ACLU')
     q = Queue(connection=conn)
     result = q.enqueue(peoplepower.queue, timeout=500)
 
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', minutes=30)
 def timed_for_indivisible():
     print('Running Job for Indivisible')
     q = Queue(connection=conn)
